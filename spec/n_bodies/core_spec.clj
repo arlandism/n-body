cd95fcebabe81336466b80732eb10cce76085f35
Arlandis Lawrence <arlandislawrence@gmail.com>
2013-08-07 14:28:56 -0500

    Adds empty z force to expected test case

--- spec/n_bodies/core_spec.clj
+++ spec/n_bodies/core_spec.clj
@@ -5,18 +5,20 @@
 (describe "force-calculator"
 
   (it "outputs x, y, and z forces"
-    (let [bodies [{:mass 100,
-                :velocity {:x 20, :y 55}
-                :position {:x 5, :y 42}},
-               {:mass 532,
-                :velocity {:x -20, :y 5}
-                :position {:x 100, :y -2}}
+    (let [bodies [{
+                :mass 100,
+                :velocity {:x 20, :y 55, :z 0}
+                :position {:x 5, :y 42, :z 0}},
+                {
+                :mass 532,
+                :velocity {:x -20, :y 5, :z 0}
+                :position {:x 100, :y -2, :z 0}}
                ]]
      (should= [{
-                :force_x 2.94E-10, :force_y -1.35E-10,
+                :force_x 2.94E-10, :force_y -1.35E-10, :force_z 0
                },
                {
-                :force_x -2.94E-10, :force_y 1.35E-10,
+                :force_x -2.94E-10, :force_y 1.35E-10, :force_z 0
                }], (force-calculator bodies))))
 
 )
