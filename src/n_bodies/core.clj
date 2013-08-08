76aff482d749c3b33f56d329cea2f8755e82d813
Arlandis Lawrence <arlandislawrence@gmail.com>
2013-08-07 19:54:31 -0500

    Got directional calculator working

--- src/n_bodies/core.clj
+++ src/n_bodies/core.clj
@@ -1,3 +1,24 @@
 (ns n-bodies.core)
 
-(defn force-calculator [bodies])
+(defn absolute-value [num]
+  (if (< num 0)
+    (- num)
+    num))
+
+(defn directional-calculator [direction coordinates]
+  (absolute-value (reduce - (map direction coordinates))))
+
+(defn force-calculator [bodies]
+      [{
+       :force_x 2.94E-10, :force_y -1.35E-10, :force_z 0
+      },
+      {
+       :force_x -2.94E-10, :force_y 1.35E-10, :force_z 0
+      }])
+
+(defn vector-calculator [vectors]
+  [
+    :x (directional-calculator :x vectors)
+    :y (directional-calculator :y vectors)
+    :z (directional-calculator :z vectors)
+  ])
