6c89a8baef17da2233c14f60cc5e7a8a70803129
Arlandis Lawrence <arlandislawrence@gmail.com>
2013-08-09 21:12:44 -0500

    Deletes some useless tests and adds a couple

--- spec/n_bodies/core_spec.clj
+++ spec/n_bodies/core_spec.clj
@@ -4,7 +4,9 @@
             [n-bodies.core :refer [force-calculator,  
                                    dimensional-difference, 
                                    force-in-dimension-on-body,
-                                   GRAVITY, force-on-one-body-from-another]]))
+                                   GRAVITY, 
+                                   sum-of-forces-on-one-body,
+                                   force-on-one-body-from-another]]))
 
 (describe "dimensional-difference"
   (it "calculates the difference between two vector directions"
@@ -85,83 +87,86 @@
                    }, 
                    (force-on-one-body-from-another one-body other-body)))))
 
+(describe "sum-of-forces-on-one-body"
+  (it "sums the forces on one body with 2 bodies total"
+    (let [one-body {
+                    :mass 5 ,
+                    :velocity {:x 20, :y 55, :z 0}
+                    :position {:x 2, :y 4, :z 4}}
+          other-body {
+                    :mass 6,
+                    :velocity {:x -20, :y 5, :z 0}
+                    :position {:x 7, :y 1, :z 0}}]
 
-;(describe "force-on-one-body"
-;  (context "two bodies"
-;    (it "calculates the force exerted on one body given index & all bodies"
-;     (let [bodies [{
-;                  :mass 100,
-;                  :velocity {:x 20, :y 55, :z 0}
-;                  :position {:x 5, :y 42, :z 0}},
-;                  {
-;                  :mass 532,
-;                  :velocity {:x -20, :y 5, :z 0}
-;                  :position {:x 100, :y -2, :z 0}
-;                  }]]
-;        (should= 
-;          {:force_x 2.94E-10, :force_y -1.35E-10, :force_z 0},
-;          (force-on-one-body 0 bodies))
-;
-;        (should= 
-;          {:force_x 2.94E-10, :force_y -1.35E-10, :force_z 0},
-;          (force-on-one-body 1 bodies))))))
+          (should= {:force_x (- (* GRAVITY (/ 6 5))), 
+                    :force_y (* GRAVITY (/ 10 3)),
+                    :force_z (* GRAVITY (/ 15 8))
+                   },
+                   (sum-of-forces-on-one-body one-body [other-body])))))
+
+  ;(it "sums the forces on one body with 3 bodies total"
+  ;  (let [one-body {
+  ;                  :mass 5 ,
+  ;                  :velocity {:x 20, :y 55, :z 0}
+  ;                  :position {:x 2, :y 4, :z 4}}
+  ;        second-body {
+  ;                  :mass 6,
+  ;                  :velocity {:x -20, :y 5, :z 0}
+  ;                  :position {:x 7, :y 1, :z 0}}
+  ;        third-body {
+  ;                  :mass 2,
+  ;                  :velocity {:x -20, :y 5, :z 0}
+  ;                  :position {:x 2, :y 1, :z 0}}]
+
+  ;        (should= {:force_x (- (* GRAVITY (/ 6 5))), 
+  ;                  :force_y (* GRAVITY (/ 40 9)),
+  ;                  :force_z (* GRAVITY (/ 5 2)) 
+  ;                 },
+  ;                 (sum-of-forces-on-one-body one-body [second-body third-body])))))
 
 (describe "force-calculator"
 
   (it "outputs x, y, and z forces given two bodies"
     (let [bodies [{
-                :mass 100,
+                :mass 3,
                 :velocity {:x 20, :y 55, :z 0}
-                :position {:x 5, :y 42, :z 0}},
+                :position {:x 2, :y 5, :z 0}},
                 {
-                :mass 532,
+                :mass 7,
                 :velocity {:x -20, :y 5, :z 0}
-                :position {:x 100, :y -2, :z 0}}]]
+                :position {:x 8, :y 3, :z 1}}]]
 
      (should= [{
-                :force_x 2.94E-10, :force_y -1.35E-10, :force_z 0
+                :force_x  (* (/ 7 -12) GRAVITY), :force_y (* (/ 21 4) GRAVITY), :force_z (* -21 GRAVITY)
                },
                {
-                :force_x -2.94E-10, :force_y 1.35E-10, :force_z 0
-               }], (force-calculator bodies)))))
+                :force_x (* (/ 7 12) GRAVITY), :force_y (* (/ -21 4) GRAVITY), :force_z (* 21 GRAVITY)
+               }], 
+              (force-calculator bodies)))))
 
-  (it "outputs x, y, and z forces given some other stuff"
-    (let [bodies [{
-                :mass 100,
-                :velocity {:x 20, :y 55, :z 0}
-                :position {:x 5, :y 42, :z 0}},
-                {
-                :mass 532,
-                :velocity {:x -20, :y 5, :z 0}
-                :position {:x 100, :y -2, :z 0}}]]
+  ;(it "outputs x, y, and z forces given three bodies"
+  ;    (let [bodies [{
+  ;                :mass 3,
+  ;                :velocity {:x 20, :y 55, :z 0}
+  ;                :position {:x 2, :y 5, :z 0}},
+  ;                {
+  ;                :mass 2,
+  ;                :velocity {:x -20, :y 5, :z 0}
+  ;                :position {:x 8, :y 3, :z 1}}
+  ;                {
+  ;                :mass 5,
+  ;                :velocity {:x 20, :y 55, :z 0}
+  ;                :position {:x 1, :y 3, :z 2}}]]
 
-     (should= [{
-                :force_x 2.94E-10, :force_y -1.35E-10, :force_z 0
-               },
-               {
-                :force_x -2.94E-10, :force_y 1.35E-10, :force_z 0
-               }], (force-calculator bodies))))
-;
-;  (it "outputs x, y, and z forces given three bodies"
-;    (let [bodies [{
-;                    :mass 200,
-;                    :velocity {:x 0, :y 0, :z 0}
-;                    :position {:x 0, :y 0, :z 0}},
-;                    {
-;                    :mass 10,
-;                    :velocity {:x 0, :y 120, :z 0}
-;                    :position {:x 160, :y 0, :z 0}}
-;                   {
-;                    :mass 0.001,
-;                    :velocity {:x 0, :y 53, :z 0}
-;                    :position {:x 140, :y 0, :z 0}}]]
-;         (should= [{
-;                    :force_x 2.94E-10, :force_y -1.35E-10, :force_z 0
-;                   },
-;                   {
-;                    :force_x -2.94E-10, :force_y 1.35E-10, :force_z 0
-;                   }
-;                   {
-;                    :force_x -2.94E-10, :force_y 1.35E-10, :force_z 0
-;                   }], (force-calculator bodies)))))
+  ;     (should= [{
+  ;                :force_x  (* (/ 89 6) GRAVITY), :force_y (* (/ 21 4) GRAVITY), :force_z (* (/ -39 4) GRAVITY)
+  ;               },
+  ;               {
+  ;                :force_x (* (/ 55 294) GRAVITY), :force_y (* (/ -4 3) GRAVITY), :force_z 0
+  ;               }
+  ;               {
+  ;                :force_x (* (/ -745 49) GRAVITY), :force_y (* (/ -15 4) GRAVITY), :force_z (* (/ -25 4 GRAVITY))
+  ;               }], 
+  ;              (force-calculator bodies)))))
+
 (run-specs)
