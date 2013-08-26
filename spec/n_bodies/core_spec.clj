36779d8472b0888a1e17e910c2e983db625db4f0
Arlandis Lawrence <arlandislawrence@gmail.com>
2013-08-26 15:11:32 -0500

    Got core file down to 45 lines

--- spec/n_bodies/core_spec.clj
+++ spec/n_bodies/core_spec.clj
@@ -3,6 +3,10 @@
             [clojure.math.numeric-tower :refer [ceil, sqrt, expt]]
             [n-bodies.core :refer :all]))
 
+;(describe "scale vector"
+;  (it "doubles the vector"
+;      (should= {:x 2 :y 4 :z 2} (scale-vector 2 {:x 1 :y 2 :z 1}))))
+
 (describe "distance-formula"
   (it "performs the distance formula for 3 dimensions"
     (should= (sqrt 2) (distance-formula {:x 1 :y 2 :z 0} {:x 2 :y 3 :z 0}))
@@ -17,180 +21,112 @@
       (should= (/ 
                  (* mass-one mass-two GRAVITY) 
                  (expt (distance-formula position-one position-two) 3)) 
-               (calculate-constant body-one body-two)))
+               (calculate-constant body-one body-two)))))
 
-    (let [mass-one 3 mass-two 4
-          position-one {:x 0 :y 0 :z 0} position-two {:x 0 :y 0 :z 0} 
-          body-one {:mass mass-one :position position-one} body-two {:mass mass-two :position position-two}]
-
-      (should= 0 (calculate-constant body-one body-two)))))
-  
-(describe "dimensional-difference"
-  (it "calculates the difference between two vector directions"
-    (let [first-vector {:x 20, :y 55, :z 0},
-          second-vector {:x -20, :y 5, :z 0}]
-      (should= 50, (dimensional-difference :y first-vector second-vector))))
-
-  (it "calculates the difference between two vector directions again"
-    (let [first-vector {:x 20, :y 55, :z 0},
-          second-vector {:x -20, :y 5, :z 0}]
-      (should= 40, (dimensional-difference :x first-vector second-vector)))))
-
-(describe "force-in-dimension-on-body"
-  (context "x dimension"
-    (it "calculates force on one body in one dimension given dimension,body,other"
-      (let [one-body {
-                     :mass 2 ,
-                     :velocity {:x 20, :y 55, :z 0}
-                     :position {:x 3, :y 4, :z 0}}
-            other-body {
-                  :mass 64,
-                  :velocity {:x -20, :y 5, :z 0}
-                  :position {:x -1, :y -2, :z 0}}]
-        (should= (* GRAVITY 8 ), (force-in-dimension-on-body :x one-body other-body)))))
-
-  (context "y dimension"
-    (it "calculates force on one body in one dimension given dimension, body, other"
-      (let [one-body {
-                     :mass 10 ,
-                     :velocity {:x 20, :y 55, :z 0}
-                     :position {:x 6, :y 4, :z 0}}
-            other-body {
-                  :mass 216,
-                  :velocity {:x -20, :y 5, :z 0}
-                  :position {:x 3, :y -2, :z 0}}]
-        (should= (* GRAVITY 60) , (force-in-dimension-on-body :y one-body other-body)))))
-
-  (context "z dimension"
-      (it "calculates force on one body in one dimension given dimension,body,other"
-        (let [one-body {
-                       :mass 10 ,
-                       :velocity {:x 20, :y 55, :z 0}
-                       :position {:x 6, :y 4, :z 55}}
-              other-body {
-                    :mass 8,
-                    :velocity {:x -20, :y 5, :z 0}
-                    :position {:x 3, :y -2, :z 57}}]
-          (should= (- (* GRAVITY 20)) , (force-in-dimension-on-body :z one-body other-body))))))
-          
 (describe "force-on-one-body-from-another"
   (it "can be used to calculate force on one body in all dimensions" 
     (let [one-body {
                    :mass 2 ,
-                   :velocity {:x 20, :y 55, :z 0}
-                   :position {:x 0, :y 3, :z 4}}
+                   :position {:x 2, :y 2 :z 2}}
           other-body {
-                :mass 4,
-                :velocity {:x -20, :y 5, :z 0}
-                :position {:x 3, :y 2, :z 1}}]
-      (should= {:x ( - (* GRAVITY (/ 8 9))), 
-                :y (* GRAVITY 8),
-                :z (* GRAVITY (/ 8 9)) 
+                :mass 1,
+                :position {:x 0 :y 0 :z 0 }}]
+      (should= {:x (/ (* GRAVITY 2 -2) (expt (sqrt 12) 3)) , 
+                :y (/ (* GRAVITY 2 -2) (expt (sqrt 12) 3)),
+                :z (/ (* GRAVITY 2 -2) (expt (sqrt 12) 3)),
                }, 
                (force-on-one-body-from-another one-body other-body))))
 
   (it "can be used to calculate force on one body in all dimensions" 
     (let [one-body   {
-                     :mass 5 ,
-                     :velocity {:x 20, :y 55, :z 0}
-                     :position {:x 2, :y 4, :z 4}
+                     :mass 1 ,
+                     :position {:x 0, :y 1, :z 2}
                      }
           other-body {
-                    :mass 6,
-                    :velocity {:x -20, :y 5, :z 0}
-                    :position {:x 7, :y 1, :z 0}
+                    :mass 2,
+                    :position {:x 2, :y 1, :z 0}
                      }
          ]
-      (should= {:x (- (* GRAVITY (/ 6 5))), 
-                :y (* GRAVITY (/ 10 3)),
-                :z (* GRAVITY (/ 15 8)) 
-               }, 
+      (should= {:x (/ (* GRAVITY 2 2) (expt (sqrt 8) 3)), 
+                :y 0.0,
+                :z  (/ (* GRAVITY 2 -2) (expt (sqrt 8) 3))
+               } 
                (force-on-one-body-from-another one-body other-body)))))
 
 (describe "sum-of-forces-on-one-body"
   (it "sums the forces on one body with 2 bodies total"
     (let [one-body   {
-                      :mass 5 
-                      :velocity {:x 20, :y 55, :z 0}
+                      :mass 3 
                       :position {:x 2, :y 4, :z 4}
                      }
           other-body {
-                      :mass 6,
-                      :velocity {:x -20, :y 5, :z 0}
+                      :mass 2,
                       :position {:x 7, :y 1, :z 0}
                      }
         ]
 
-          (should= {:x (- (* GRAVITY (/ 6 5))), 
-                    :y (* GRAVITY (/ 10 3)),
-                    :z (* GRAVITY (/ 15 8))
+          (should= {:x (/ (* GRAVITY 6 5) (expt (sqrt 50) 3)) 
+                    :y (/ (* GRAVITY 6 -3) (expt (sqrt 50) 3))
+                    :z (/ (* GRAVITY 6 -4) (expt (sqrt 50) 3))
                    }
                    (sum-of-forces-on-one-body one-body [other-body]))
 
-          (should= {:x (* GRAVITY (/ 6 5)), 
-                    :y (* GRAVITY (/ 10 -3)),
-                    :z (* GRAVITY (/ 15 -8))
+          (should= {:x (/ (* GRAVITY 6 -5) (expt (sqrt 50) 3)) 
+                    :y (/ (* GRAVITY 6 3) (expt (sqrt 50) 3))
+                    :z (/ (* GRAVITY 6 4) (expt (sqrt 50) 3))
                    }
                    (sum-of-forces-on-one-body other-body [one-body]))))
 
   (it "sums the forces on one body with 3 bodies total"
     (let [one-body    {
-                       :mass 5 ,
-                       :velocity {:x 20, :y 55, :z 0}
+                       :mass 2
                        :position {:x 2, :y 4, :z 4}
                       } 
           second-body {
-                      :mass 6,
-                      :velocity {:x -20, :y 5, :z 0}
+                      :mass 1
                       :position {:x 7, :y 1, :z 0}
                       }  
           third-body  {
-                      :mass 2,
-                      :velocity {:x -20, :y 5, :z 0} 
+                      :mass 2
                       :position {:x 2, :y 2, :z 0}
                       }
         ] 
 
-          (should= {:x (- (* GRAVITY (/ 6 5))), 
-                    :y (* GRAVITY (/ 35 6)),
-                    :z (* GRAVITY (/ 5 2)) 
+          (should= {:x (+ (/ (* GRAVITY 2 5) (expt (sqrt 50) 3)))  
+                    :y (+ (/ (* GRAVITY 2 -3) (expt (sqrt 50) 3)) (/ (* GRAVITY 4 -2) (expt (sqrt 20) 3)))
+                    :z (+ (/ (* GRAVITY 2 -4) (expt (sqrt 50) 3)) (/ (* GRAVITY 4 -4) (expt (sqrt 20) 3)))
                    }
                    (sum-of-forces-on-one-body one-body [second-body third-body])))))
-  
+
 (describe "compute-forces"
   (it "outputs x, y, and z forces given two bodies"
     (let [bodies [{
                     :mass 3,
-                    :velocity {:x 20, :y 55, :z 0}
                     :position {:x 2, :y 5, :z 0}},
                   {
                     :mass 7,
-                    :velocity {:x -20, :y 5, :z 0}
                     :position {:x 8, :y 3, :z 1}
                   }]
          ]
 
      (should= [
-               (  sum-of-forces-on-one-body (first bodies) [(second bodies)])
-               (  sum-of-forces-on-one-body (second bodies) [(first bodies)]) 
+               (sum-of-forces-on-one-body (first bodies) [(second bodies)])
+               (sum-of-forces-on-one-body (second bodies) [(first bodies)]) 
               ]
               (compute-forces bodies))))
 
   (it "outputs x, y, and z forces given three bodies"
       (let [bodies [{
                       :mass 3,
-                      :velocity {:x 20, :y 55, :z 0}
                       :position {:x 2, :y 5, :z 0}
                     }
                     {
-                      :mass 5,
-                      :velocity {:x 20, :y 55, :z 0}
+                      :mass 1,
                       :position {:x 1, :y 3, :z 2}
                     }
                     {
                       :mass 2,
-                      :velocity {:x -20, :y 5, :z 0}
-                      :position {:x 8, :y 3, :z 1}
+                      :position {:x 1, :y 3, :z 1}
                     }]
           ]
        (should= [
@@ -199,9 +135,5 @@
                  (sum-of-forces-on-one-body (bodies 2) [(bodies 0) (bodies 1)])
                 ] 
                 (compute-forces bodies)))))
-
-(describe "scale vector"
-  (it "doubles the vector"
-      (should= {:x 2 :y 4 :z 2} (scale-vector 2 {:x 1 :y 2 :z 1}))))
 
 (run-specs)
--- src/n_bodies/core.clj
+++ src/n_bodies/core.clj
@@ -6,67 +6,31 @@
 (defn move-head-to-tail [coll]
   (drop 1 (conj coll (first coll))))
 
-(defn scale-vector [scale v]
-  {:x (* scale (v :x))
-   :y (* scale (v :y))
-   :z (* scale (v :z))})
+(defn compute-f-on-dimensions [f first-coordinate second-coordinate]
+  (into {} (for [dimension [:x :y :z]]
+    [dimension (f (first-coordinate dimension) (second-coordinate dimension))]))) 
 
-(defn force-template [x y z]
-  {:x x
-   :y y
-   :z z})
+(defn scale-vector [scale vector]
+  (into {} (for [[dimension value] vector] [dimension (* scale value)])))
 
 (defn distance-formula [pos-one pos-two]
-  (let [a-squared (expt (- (pos-one :x) (pos-two :x)) 2)
-        b-squared (expt (- (pos-one :y) (pos-two :y)) 2)
-        c-squared (expt (- (pos-one :z) (pos-two :z)) 2)]
-  (sqrt
-    (+ a-squared b-squared c-squared))))
+  (sqrt (reduce + (for [dimension (keys pos-one)]
+    (expt (- (pos-one dimension) (pos-two dimension)) 2)))))
 
 (defn calculate-constant [body-one body-two]
   (let [distance (distance-formula (body-one :position) (body-two :position))]
-    (if (zero? distance) 
-      0
       (/
         (* GRAVITY (body-one :mass) (body-two :mass))
-        (expt distance 3)))))
-
- (defn dimensional-difference [direction first-coordinate second-coordinate]
-  (reduce - (map direction [first-coordinate second-coordinate])))
-
-(defn vector-difference [first-coordinate second-coordinate]
-  (force-template
-    (- (first-coordinate :x) (second-coordinate :x))
-    (- (first-coordinate :y) (second-coordinate :y))
-    (- (first-coordinate :z) (second-coordinate :z))))
-
-(defn force-in-dimension-on-body [dimension target-body other-body]
-  (let [distance (dimensional-difference dimension (target-body :position) (other-body :position))
-        product-of-masses (reduce * (map :mass [target-body other-body]))
-        distance-cubed (expt (abs distance) 3)]
-    (if (zero? distance-cubed)
-      0
-      (/ (* distance product-of-masses GRAVITY) distance-cubed))))
+        (expt distance 3))))
 
 (defn force-on-one-body-from-another [body-one body-two]
-  ;scale-vector 
-  ;  (calculate-constant body-one body-two) 
-  ;  (vector-difference (body-one :position) (body-two :position)))
-
-   (force-template
-     (force-in-dimension-on-body :x body-one body-two) 
-     (force-in-dimension-on-body :y body-one body-two)
-     (force-in-dimension-on-body :z body-one body-two)))
-
-(defn sum-forces [force-one force-two]
-  (force-template
-    (reduce + (map :x [force-one  force-two])) 
-    (reduce + (map :y [force-one  force-two])) 
-    (reduce + (map :z [force-one force-two]))))
+  (scale-vector 
+    (calculate-constant body-one body-two) 
+    (compute-f-on-dimensions - ( body-two :position) ( body-one :position)))) 
 
 (defn sum-of-forces-on-one-body [body-one rest-of-bodies]
   (reduce (fn [forces-so-far current-body]
-            (sum-forces
+            (compute-f-on-dimensions +
               forces-so-far
               (force-on-one-body-from-another body-one current-body)))
           {:x 0 :y 0 :z 0}
